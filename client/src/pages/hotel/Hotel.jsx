2b882ed3a1240954c17a47ff3ebbceac7ffc1c47
Nami Miyashiro <nami.miyashiro@gmail.com>
2023-08-20 17:02:22 -0700

    issues with login

--- client/src/pages/hotel/Hotel.jsx
+++ client/src/pages/hotel/Hotel.jsx
@@ -14,6 +14,7 @@ import { useContext, useState } from "react";
 import useFetch from "../../hooks/useFetch";
 import { useLocation, useNavigate } from "react-router-dom";
 import { SearchContext } from "../../context/SearchContext";
+import { daysInYear } from "date-fns/fp";
 
 const Hotel = () => {
   const location = useLocation();
@@ -23,8 +24,17 @@ const Hotel = () => {
 
   const { data, loading, error } = useFetch(`/hotels/find/${id}`);
 
-  const { dates } = useContext(SearchContext);
+  const { dates, options } = useContext(SearchContext);
   console.log(dates);
+
+  const MILLISECONDS_PER_DAY = 1000 * 60 * 60 * 24;
+  function dayDifference(date1, date2) {
+    const timeDiff = Math.abs(date2.getTime() - date1.getTime());
+    const diffDays = Math.ceil(timeDiff / MILLISECONDS_PER_DAY);
+    return diffDays;
+  }
+
+  const days = dayDifference(dates[0].endDate, dates[0].startDate);
 
   const handleOpen = (i) => {
     setSlideNumber(i);
@@ -110,13 +120,13 @@ const Hotel = () => {
                 </p>
               </div>
               <div className="hotelDetailsPrice">
-                <h1>Perfect for a 9-night stay!</h1>
+                <h1>Perfect for a {days}-night stay!</h1>
                 <span>
                   Located in the real heart of Krakow, this property has an
                   excellent location score of 9.8!
                 </span>
                 <h2>
-                  <b>$945</b> (9 nights)
+                  <b>${days * data.cheapestPrice * options.room}</b> ({days} nights)
                 </h2>
                 <button>Reserve or Book Now!</button>
               </div>
